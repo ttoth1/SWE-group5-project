d132e9daa70dfd355142d9c3def337efbf5eed20
Trevor Toth <trevor.toth11@gmail.com>
2022-04-01 12:30:53 -0400

    uncommented port for heroku deployment

--- routes.py
+++ routes.py
@@ -170,6 +170,6 @@ def index():
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
-        # port=int(os.getenv("PORT", 8080)),
+        port=int(os.getenv("PORT", 8080)),
         debug=True,
     )
