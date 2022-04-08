76ee6922fad82e2c96991e69da7e4eb0402dd243
Trevor Toth <trevor.toth11@gmail.com>
2022-04-08 12:12:23 -0400

    Added spotify react login page with artist search functionality

--- routes.py
+++ routes.py
@@ -21,8 +21,8 @@ bp = flask.Blueprint(
 )
 
 
-@bp.route("/new_page")
-def new_page():
+@bp.route("/spotify_login")
+def spotify_login():
     return flask.render_template("index.html")
 
 
@@ -190,7 +190,7 @@ def index():
 
 if __name__ == "__main__":
     app.run(
-        # host=os.getenv("IP", "0.0.0.0"),
-        # port=int(os.getenv("PORT", 8080)),
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8080)),
         debug=True,
     )
--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import axios from "axios"
 
 function App() {
   const CLIENT_ID = "c96716b2eba14725b66e4cde692c3d22"
-  const REDIRECT_URI = "http://127.0.0.1:5000/new_page"
+  const REDIRECT_URI = "https://cherry-pie-58273.herokuapp.com/spotify_login"
+  // const REDIRECT_URI = "http://127.0.0.1:5000/spotify_login"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
   const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
 
   useEffect(() => {
     const hash = window.location.hash
@@ -19,13 +22,39 @@ function App() {
 
       window.location.hash = ""
       window.localStorage.setItem("token", token)
-      setToken(token)
     }
+    setToken(token)
+
   }, [])
 
   const logout = () => {
     setToken("")
     window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params:{
+        q: searchKey,
+        type: "artist"
+      }
+    })
+    setArtists(data.artists.items)
+    console.log("searchArtists() called");
+  }
+
+  const renderArtists = () => {
+    console.log("renderArtists() called");
+    return artists.map(artist => (
+      <div key={artist.id}>
+        {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+        {artist.name}
+      </div>
+    ))
   }
 
   return (
@@ -35,6 +64,16 @@ function App() {
         {!token ?
           <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
           : <button onClick={logout}>Logout</button>}
+
+        {token ?
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Artist search</button>
+          </form>
+          : <h2>Please login</h2>
+        }
+
+        {renderArtists()}
       </header>
     </div>
   );
