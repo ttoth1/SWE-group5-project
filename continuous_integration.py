3204efe4535a48152a0bd678df53a3353965a887
Habeeb <mhabeeb1@DESKTOP-25IBTLF.localdomain>
2022-04-21 22:35:42 -0400

    Adding Server test for Spotify API

--- continuous_integration.py
+++ continuous_integration.py
@@ -2,6 +2,7 @@ import unittest
 from flask import url_for, request
 from functions import encrypt_password
 from spotify_model import load_spotify_features
+from get_track_info import get_track_info
 
 class testPasswordEncryption(unittest.TestCase):
     
@@ -20,3 +21,13 @@ class testPasswordEncryption(unittest.TestCase):
         expected_output = ('<class \'pandas.core.frame.DataFrame\'>',
         '<class \'pandas.core.frame.DataFrame\'>')
         self.assertEqual(expected_output,actual_output)
+    
+
+    ## Server Client Test for Spotify APU
+    def testSpotifyGetTrackInfo(self):
+        test_track_id = '3roq8qsVwzSEQA2lorvJEG'
+        expected_output = ['Starting Over', 'https://open.spotify.com/track/3roq8qsVwzSEQA2lorvJEG', 'Beres Hammond', 'https://open.spotify.com/artist/2ruMkdO4e1tJWDHsYSEtxr', 'Beres Hammond Classic Songs', 'https://open.spotify.com/album/71bmlG2MmQhqYfR2ENeKwp', 'https://i.scdn.co/image/ab67616d00001e0245684482da59fdce28c8be2c']
+        track_name, track_link, artist, artist_link, album,  album_link, album_pic = get_track_info(test_track_id)
+        output_arr = [track_name, track_link, artist, artist_link, album,  album_link, album_pic]
+        self.assertEqual(expected_output,output_arr)
+
